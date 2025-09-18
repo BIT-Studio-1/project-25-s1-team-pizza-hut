077780dd30b2e5ff63a064b776778ad0baf0c0d2
Corey Jones (1000134581) <JONECJ3@student.op.ac.nz>
2025-09-18 16:29:55 +1200

    Added VCode and Document edits

---
+++ ASCII GAME/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ASCII_GAME
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Game Code
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
